d3031f3b768cbc5b429a545d9f56a895af5a177f
Gustavo Silveira <stulpg@gmail.com>
2026-05-04 19:30:07 -0300

    Introduzindo HTML na lista de aprendizados

---
+++ HTML/Rascunhos/SobreMim.js
@@ -0,0 +1,13 @@
+let x = 0; // contador
+
+function TrocarFoto(){ // function que foi criada baseada no HTML, junto com o ID
+    let img = document.getElementById("MinhaFoto");
+
+    if (x === 0){ // Quando clicar na foto, vai mudar de uma pra outra
+        img.src = "img/Foto2.jpeg";
+        x = 1;
+    } else {
+        img.src = "img/Foto1.jpeg";
+        x = 0;
+    }
+} 
